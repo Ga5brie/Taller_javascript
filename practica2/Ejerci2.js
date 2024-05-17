f0071a684f015a94c9202c49e8257d5e288b80e1
ga5brie <junsumy523@gmail.com>
2024-05-16 23:15:16 -0500

    USO DE BUCLES

---
+++ practica2/Ejerci2.js
@@ -0,0 +1,9 @@
+// for (let i=0; 1<=10; i++){
+    //inicio final como quieres incrementar
+    //console.log(i);
+//}
+let suma=0;
+for(let i=1; i<=100; i++){
+    suma+=i;
+}
+console.log("LA SUMA DEL DE LOS NUMEROS DEL 1 AL 100 ES: ",suma);
